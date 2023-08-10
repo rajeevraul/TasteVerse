af9de14632695540a076c0e241df6bb5fe69495d
Tharaa40 <95153704+Tharaa40@users.noreply.github.com>
2023-08-10 15:42:57 +0800

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -90,36 +90,36 @@ router.get("/list", (req,res) => {
 
 ///////////////////////////////////////////// HELPERS ///////////////////////////////////////////
 
-/**
- * @desc A helper function to generate a random string
- * @returns a random lorem ipsum string
- */
-function generateRandomData(numWords = 5) {
-  const str =
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+// /**
+//  * @desc A helper function to generate a random string
+//  * @returns a random lorem ipsum string
+//  */
+// function generateRandomData(numWords = 5) {
+//   const str =
+//     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
 
-  const words = str.split(" ");
+//   const words = str.split(" ");
 
-  let output = "";
+//   let output = "";
 
-  for (let i = 0; i < numWords; i++) {
-    output += choose(words);
-    if (i < numWords - 1) {
-      output += " ";
-    }
-  }
+//   for (let i = 0; i < numWords; i++) {
+//     output += choose(words);
+//     if (i < numWords - 1) {
+//       output += " ";
+//     }
+//   }
 
-  return output;
-}
+//   return output;
+// }
 
-/**
- * @desc choose and return an item from an array
- * @returns the item
- */
-function choose(array) {
-  assert(Array.isArray(array), "Not an array");
-  const i = Math.floor(Math.random() * array.length);
-  return array[i];
-}
+// /**
+//  * @desc choose and return an item from an array
+//  * @returns the item
+//  */
+// function choose(array) {
+//   assert(Array.isArray(array), "Not an array");
+//   const i = Math.floor(Math.random() * array.length);
+//   return array[i];
+// }
 
 module.exports = router;
